423d1d6d379ce1730de7cd82a3366efefad2d038
mdislammazharul <mazharul.py@gmail.com>
2022-08-21 17:27:27 +0200

    for-compute

---
+++ Vue Js-3/app.js
@@ -0,0 +1,28 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        items: [
+            { name: 'Rice', price: 50 },
+            { name: 'Chicken', price: 200 },
+            { name: 'Egg', price: 150 },
+        ],
+        newItem: ''
+    },
+    computed: {
+        total() {
+            var sum = 0
+            this.items.forEach(item => {
+                sum += parseFloat(item.price)
+            });
+            return sum
+        }
+    },
+    methods: {
+        addItem() {
+            this.items.push({
+                name: this.newItem,
+                price: 0.0
+            })
+        },
+    }
+})
