225fe9ad000aadd3d529fb4fa9bdc1045bcfa47b
mdislammazharul <mazharul.py@gmail.com>
2022-10-03 10:16:42 +0200

    vue_3.2.0

---
+++ Vue Js-7/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp ({
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon Sadnderson',
+            age: 45
+        }
+    }
+})
+
+app.mount('#app')
