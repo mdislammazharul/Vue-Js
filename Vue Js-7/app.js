5bc2528775b8c5cc5468f4edf07f770bd922c6d8
mdislammazharul <mazharul.py@gmail.com>
2022-10-04 09:49:55 +0200

    mouse event

--- Vue Js-7/app.js
+++ Vue Js-7/app.js
@@ -5,12 +5,20 @@ const app = Vue.createApp ({
             title: 'The Final Empire',
             author: 'Brandon Sadnderson',
             age: 45,
-            
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent (e) {
+            console.log(e)
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX,
+            this.y = e.offsetY
         }
     }
 })
